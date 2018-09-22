6a49de659c9c0de482cfe10d8d809b81da696022
Nghiep Vo <vokenghiep@gmail.com>
2018-09-22 21:29:34 +0700

    Study EF core

---
+++ part3/ef/Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Run
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
